1f5fc81698e4db739eff4e51890a0fadd32baa90
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-01-21 18:10:20 +0000

    uprage make negative

--- codewars-draft/01-tasks/make-negative.js
+++ codewars-draft/01-tasks/make-negative.js
@@ -15,7 +15,21 @@ function makeNegative(num) {
 //Мучкін Олександр
 
 //Радіонов Павло
-
+function makeNegative(num) {
+  // тут перевіряю, чи число вже є від'ємним
+  if (num > 0) {
+      // Якщо так, то змінюю його на від'ємне
+      return -num;
+  } else {
+      // Якщо воно вже від'ємне або дорівнює нулю, повертаю його без змін
+      return num;
+  }
+}
+// використання:
+console.log(makeNegative(1));    // вивід: -1
+console.log(makeNegative(-5));   // вивід: -5
+console.log(makeNegative(0));    // вивід: 0
+console.log(makeNegative(0.12)); // вивід: -0.12
 //Волохов Антон
 
 //Затонський Степан 
