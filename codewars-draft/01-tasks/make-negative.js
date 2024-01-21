c034d780023bc6e6987cc507afe5fe22d080d11c
dari-ko <154279281+dari-ko@users.noreply.github.com>
2024-01-21 23:58:38 +0000

    add to make-negative draft

--- codewars-draft/01-tasks/make-negative.js
+++ codewars-draft/01-tasks/make-negative.js
@@ -5,16 +5,20 @@ function makeNegative(num) {
   return num < 0 ? num : -num;
 }
 //Токарева Дар'я
-
+function makeNegative(num) {
+  if (num > 0) {
+    return num * -1;
+  } else {
+    return num;
+  }
+}
 //Юрій Світуха
 
 //Ярема Наталія
 function makeNegative(num) {
-  if (num>0)
-    return (-num)
-  else
-   return (num)   
-  }
+  if (num > 0) return -num;
+  else return num;
+}
 //Михальська Мар'яна
 
 //Мучкін Олександр
@@ -23,18 +27,18 @@ function makeNegative(num) {
 function makeNegative(num) {
   // тут перевіряю, чи число вже є від'ємним
   if (num > 0) {
-      // Якщо так, то змінюю його на від'ємне
-      return -num;
+    // Якщо так, то змінюю його на від'ємне
+    return -num;
   } else {
-      // Якщо воно вже від'ємне або дорівнює нулю, повертаю його без змін
-      return num;
+    // Якщо воно вже від'ємне або дорівнює нулю, повертаю його без змін
+    return num;
   }
 }
 // використання:
-console.log(makeNegative(1));    // вивід: -1
-console.log(makeNegative(-5));   // вивід: -5
-console.log(makeNegative(0));    // вивід: 0
+console.log(makeNegative(1)); // вивід: -1
+console.log(makeNegative(-5)); // вивід: -5
+console.log(makeNegative(0)); // вивід: 0
 console.log(makeNegative(0.12)); // вивід: -0.12
 //Волохов Антон
 
-//Затонський Степан 
+//Затонський Степан
