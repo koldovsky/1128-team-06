67cbaef63fa5777ed0d4e5801b42c157488625f1
stevepjazz <144064883+stevepjazz@users.noreply.github.com>
2024-01-22 00:07:42 +0200

    Update and rename Opposites Attract.js to opposites-attract-sz.js

--- codewars-draft/01-tasks/opposites-attract-sz.js
+++ codewars-draft/01-tasks/opposites-attract-sz.js
@@ -1,4 +1,4 @@
-// Opposites Attract
+// Opposites Attract SZ
 function lovefunc(flower1, flower2) {
     if ((flower1 % 2 === 0 && flower2 % 2 === 0) || (flower1 % 2 !== 0 && flower2 % 2 !== 0)) {
         return 'false';
