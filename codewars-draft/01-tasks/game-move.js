ea2f1b132bb9e4f62fb77b3aa627f23701bad762
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-01-21 18:11:32 +0000

    uprage game move

--- codewars-draft/01-tasks/game-move.js
+++ codewars-draft/01-tasks/game-move.js
@@ -15,7 +15,13 @@ function move (position, roll) {
 //Мучкін Олександр
 
 //Радіонов Павло
-
+function move(position, roll) {
+  // Розраховую положення нового героя
+  var newPosition = position + roll * 2;
+  return newPosition;
+}
+// вивід:
+console.log(move(3, 6)); // Вивід: 15
 //Волохов Антон
 
 //Затонський Степан 
