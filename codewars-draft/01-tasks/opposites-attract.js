7858822c3a21e12d4cd6a387960d9a7e10010a47
dari-ko <154279281+dari-ko@users.noreply.github.com>
2024-01-22 01:08:55 +0000

    add to opposites-attract

--- codewars-draft/01-tasks/opposites-attract.js
+++ codewars-draft/01-tasks/opposites-attract.js
@@ -1,23 +1,29 @@
 //Тимчук Юрій
 
 //Ткач Євген
-function lovefunc(flower1, flower2){
+function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
 }
 //Токарева Дар'я
-
+function lovefunc(flower1, flower2) {
+  return (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 === 0)
+    ? true
+    : false;
+}
 //Юрій Світуха
 
 //Ярема Наталія
-function lovefunc(flower1, flower2){
-  if (flower1 %2===0 && flower2%2 !==0){
-  return true;
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+    return true;
   }
-  if (flower1%2 !==0 && flower2 %2===0){
-  return true;
+  if (flower1 % 2 !== 0 && flower2 % 2 === 0) {
+    return true;
+  } else {
+    return false;
   }
-  else {return false}
-  }
+}
 //Михальська Мар'яна
 
 //Мучкін Олександр
@@ -25,7 +31,10 @@ function lovefunc(flower1, flower2){
 //Радіонов Павло
 function lovefunc(flower1, flower2) {
   // Перевіряю, чи кількість пелюсток різна за парність
-  return (flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0);
+  return (
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 === 0)
+  );
 }
 // використання:
 console.log(lovefunc(2, 3)); // Вивід: true
@@ -33,4 +42,4 @@ console.log(lovefunc(4, 6)); // Вивід: false
 console.log(lovefunc(7, 8)); // Вивід: true
 //Волохов Антон
 
-//Затонський Степан 
+//Затонський Степан
