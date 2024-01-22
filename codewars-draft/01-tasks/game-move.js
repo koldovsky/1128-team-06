2ac22bd6955679b70a09229766078e6ea23dd51a
Pavlo <155533645+pavlo768@users.noreply.github.com>
2024-01-22 12:05:02 +0000

    minor change game move

--- codewars-draft/01-tasks/game-move.js
+++ codewars-draft/01-tasks/game-move.js
@@ -20,11 +20,9 @@ function move(position, roll) {
 
 //Радіонов Павло
 function move(position, roll) {
-  // Розраховую положення нового героя
   var newPosition = position + roll * 2;
   return newPosition;
 }
-// вивід:
 console.log(move(3, 6)); // Вивід: 15
 //Волохов Антон
 
