b9db23df76503d3b6a720489a15f099801a3c9b7
Yevhen Tkach <150246856+jimmy2jk@users.noreply.github.com>
2024-01-22 13:20:50 +0200

    Update opposites-attract.js

--- codewars-draft/01-tasks/opposites-attract.js
+++ codewars-draft/01-tasks/opposites-attract.js
@@ -43,3 +43,15 @@ console.log(lovefunc(7, 8)); // Вивід: true
 //Волохов Антон
 
 //Затонський Степан
+function lovefunc(flower1, flower2) {
+    if ((flower1 % 2 === 0 && flower2 % 2 === 0) || (flower1 % 2 !== 0 && flower2 % 2 !== 0)) {
+        return 'false';
+    } else {
+        return 'true';
+    }
+}
+let x = lovefunc(1, 2);
+console.log(x);
+
+// якщо обидва аргумента функції діляться на два без остачі або з остачею одночасно,
+// вони не закохані, а це просто симпатія( 
