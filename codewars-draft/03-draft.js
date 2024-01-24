ff1612dd5bd52a42e5cc985935a4a762f04e9a21
dari-ko <154279281+dari-ko@users.noreply.github.com>
2024-01-24 18:28:23 +0000

    create 03.js

---
+++ codewars-draft/03-draft.js
@@ -0,0 +1,131 @@
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+//Find the smallest integer in the array
+
+//Тимчук Юрій
+
+//Ткач Євген
+
+//Токарева Дар'я
+
+//Юрій Світуха
+
+//Ярема Наталія
+
+//Михальська Мар'яна
+
+//Мучкін Олександр
+
+//Радіонов Павло
+
+//Волохов Антон
+
+//Затонський Степан
+
+//Олексій Севастьянов
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+//Geometry Basics: Circle Circumference in 2D
+
+
+//Тимчук Юрій
+
+//Ткач Євген
+
+//Токарева Дар'я
+
+//Юрій Світуха
+
+//Ярема Наталія
+
+//Михальська Мар'яна
+
+//Мучкін Олександр
+
+//Радіонов Павло
+
+//Волохов Антон
+
+//Затонський Степан
+
+//Олексій Севастьянов
+
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+//Training JS #12: loop statement --for..in and for..of
+
+
+//Тимчук Юрій
+
+//Ткач Євген
+
+//Токарева Дар'я
+
+//Юрій Світуха
+
+//Ярема Наталія
+
+//Михальська Мар'яна
+
+//Мучкін Олександр
+
+//Радіонов Павло
+
+//Волохов Антон
+
+//Затонський Степан
+
+//Олексій Севастьянов
+
+
+// =============================== Поглиблені задачки ===================================
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+//Understanding closures - the basics
+
+
+//Тимчук Юрій
+
+//Ткач Євген
+
+//Токарева Дар'я
+
+//Юрій Світуха
+
+//Ярема Наталія
+
+//Михальська Мар'яна
+
+//Мучкін Олександр
+
+//Радіонов Павло
+
+//Волохов Антон
+
+//Затонський Степан
+
+//Олексій Севастьянов
+
+
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+//Fun with ES6 Classes #2 - Animals and Inheritance
+
+//Тимчук Юрій
+
+//Ткач Євген
+
+//Токарева Дар'я
+
+//Юрій Світуха
+
+//Ярема Наталія
+
+//Михальська Мар'яна
+
+//Мучкін Олександр
+
+//Радіонов Павло
+
+//Волохов Антон
+
+//Затонський Степан
+
+//Олексій Севастьянов
---
+++ codewars-tasks/tasks-03.js
@@ -0,0 +1,22 @@
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+//Find the smallest integer in the array
+
+
+//https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+//Geometry Basics: Circle Circumference in 2D
+
+
+
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+//Training JS #12: loop statement --for..in and for..of
+
+
+
+// =============================== Поглиблені задачки ===================================
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+//Understanding closures - the basics
+
+
+
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+//Fun with ES6 Classes #2 - Animals and Inheritance
