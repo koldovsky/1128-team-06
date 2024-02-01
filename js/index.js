00213b6c6ee537960ae64462a88953983d97a292
Natusiayarema <145716646+Natusiayarema@users.noreply.github.com>
2024-02-01 20:59:05 +0000

    JS

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ document.addEventListener("partialsLoaded", () => {
   import("./header.js");
   import("./fave-finds.js");
   import("./about-us-accordion.js");
-  
+  import("./index-top-collection.js")
 });
