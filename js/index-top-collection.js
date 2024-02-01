0794baa985b205da230bc4ae0638d1a401def013
Natusiayarema <145716646+Natusiayarema@users.noreply.github.com>
2024-02-01 21:06:15 +0000

    js

--- js/index-top-collection.js
+++ js/index-top-collection.js
@@ -1,1 +1,5 @@
-const clockContainer=document.querySelector('.')
+const clockContainer=document.querySelector('.top-collection-clock');
+function updateClock() {
+    clockContainer.innerText=new Date().toLocaleTimeString('uk');
+}
+setInterval(updateClock,1000);
