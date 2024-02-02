28302d45860a90ce87a70d59a91790dd937977f5
Natusiayarema <145716646+Natusiayarema@users.noreply.github.com>
2024-02-02 00:14:58 +0000

    last

--- js/index-top-title.js
+++ js/index-top-title.js
@@ -1,5 +1,5 @@
 const TopCollectionTitle=document.querySelector('.topCollection__title');
 function blinkText() {
-title.classList.add('highlight');
+title.classList.toggle('highlight');
 }
 setInterval(blinkText,1000);
