87da70ed7db3cd972e7d6bb9f3ce757692c1a89c
Natusiayarema <145716646+Natusiayarema@users.noreply.github.com>
2024-02-02 22:29:05 +0000

    clock

--- js/index-promotion-inverse-time.js
+++ js/index-promotion-inverse-time.js
@@ -1,20 +1,26 @@
-const endOfSaleDate = new Date('2024-02-29T23:59:59');
+const clockContainer = document.querySelector('.top-collection__inverseclock');
+const endOfSaleDate = new Date('2024-02-29T23:59:59'); // Замініть це значення на дату завершення розпродажу
 
-    function calculateTimeRemaining() {
-      const currentDate = new Date();
-      const timeDifference = endOfSaleDate - currentDate;
+function calculateTimeRemaining() {
+  const currentDate = new Date();
+  const timeDifference = endOfSaleDate - currentDate;
 
-      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
-      return { days, hours, minutes };
-    }
+  return { days, hours, minutes };
+}
 
-    function updateTimer() {
-      const timeRemaining = calculateTimeRemaining();
-      const timerElement = document.getElementById('timer');
-      timerElement.textContent = `Залишилося ${timeRemaining.days} днів, ${timeRemaining.hours} годин, ${timeRemaining.minutes} хвилин до кінця розпродажу.`;
-    }
+function updateTimer() {
+  const timeRemaining = calculateTimeRemaining();
+  const daysElement = clockContainer.querySelector('#days');
+  const hoursElement = clockContainer.querySelector('#hours');
+  const minutesElement = clockContainer.querySelector('#minutes');
 
-        setInterval(updateTimer, 1000);
+  daysElement.textContent = timeRemaining.days;
+  hoursElement.textContent = timeRemaining.hours;
+  minutesElement.textContent = timeRemaining.minutes;
+}
+
+setInterval(updateTimer, 1000);
