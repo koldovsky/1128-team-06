5ec841b8c6c289f34d8e598e2fe1a372fb88a557
jimmy2jk <zhekatkach04@gmail.com>
2024-02-02 13:00:43 +0200

    Slick carousel fix

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ document.addEventListener("partialsLoaded", () => {
   import("./fave-finds.js");
   import("./about-us-accordion.js");
   import("./slick-quotes-carousel.js");
-  import("./slick.min.js");
   import("./index-top-clock.js");
   import("./index-top-title.js");
 });
